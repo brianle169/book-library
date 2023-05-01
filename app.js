9ddfe6f2fd90e5baf8cad6e6538535771b404058
Brian Le <83758821+brianle169@users.noreply.github.com>
2023-05-02 00:21:09 +0700

    Add feature: toggle .add-new-book form

--- app.js
+++ app.js
@@ -14,3 +14,20 @@ Book.prototype.logInfo = function () {
 function addBookToLibrary() {
 	// Add book to library
 }
+
+const closeFormButton = document.querySelector(".close-form > img");
+const openFormButton = document.querySelector(".add-book > img");
+const addBookForm = document.querySelector(".add-book-form");
+const container = document.querySelector(".container");
+
+function toggleAddBookForm(event) {
+	if (event.target === openFormButton) {
+		addBookForm.style = "display: flex;";
+		container.style = "filter: blur(5px);";
+	} else if (event.target === closeFormButton) {
+		addBookForm.style = "display: flex;";
+		container.style = "";
+	}
+}
+openFormButton.addEventListener("click", toggleAddBookForm);
+closeFormButton.addEventListener("click", toggleAddBookForm);
